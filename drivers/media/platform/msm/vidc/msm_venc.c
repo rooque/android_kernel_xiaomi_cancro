7b4e5d07623abb61a5bd88c55f446305a1c88681
Ashray Kulkarni <ashrayk@codeaurora.org>
2013-11-12 19:04:02 -0800

    msm: vidc: add LTR support for video encoder
    Enable LTR support for VP8 codec to add robust error resilience.
    
    Change-Id: I3b87c5cdaef836a6d02d5b9fe71b7165b7f13581
    Signed-off-by: Ashray Kulkarni <ashrayk@codeaurora.org>
    
    Conflicts:
    	drivers/media/platform/msm/vidc/msm_venc.c
    
    Conflicts:
    	drivers/media/platform/msm/vidc/hfi_packetization.c
    	drivers/media/platform/msm/vidc/msm_venc.c
    	drivers/media/platform/msm/vidc/msm_vidc_common.c
    	drivers/media/platform/msm/vidc/vidc_hfi.h
    	drivers/media/platform/msm/vidc/vidc_hfi_api.h
    	drivers/media/platform/msm/vidc/vidc_hfi_helper.h
    	include/linux/videodev2.h

--- drivers/media/platform/msm/vidc/msm_venc.c
+++ drivers/media/platform/msm/vidc/msm_venc.c
@@ -761,6 +761,7 @@ static struct msm_vidc_ctrl msm_venc_ctrls[] = {
 		.default_value = 0,
 		.step = 1,
 		.qmenu = NULL,
+		.cluster = MSM_VENC_CTRL_CLUSTER_USE_LTRFRAME,
 	},
 	{
 		.id = V4L2_CID_MPEG_VIDC_VIDEO_LTRMODE,
@@ -771,6 +772,7 @@ static struct msm_vidc_ctrl msm_venc_ctrls[] = {
 		.default_value = V4L2_MPEG_VIDC_VIDEO_LTR_MODE_DISABLE,
 		.step = 1,
 		.qmenu = NULL,
+		.cluster = MSM_VENC_CTRL_CLUSTER_USE_LTRFRAME,
 	},
 	{
 		.id = V4L2_CID_MPEG_VIDC_VIDEO_MARKLTRFRAME,
@@ -2149,6 +2151,15 @@ static int try_set_ctrl(struct msm_vidc_inst *inst, struct v4l2_ctrl *ctrl)
 	return rc;
 }
 
+static struct v4l2_ctrl *get_cluster_from_id(int id)
+{
+	int c;
+	for (c = 0; c < ARRAY_SIZE(msm_venc_ctrls); ++c)
+		if (msm_venc_ctrls[c].id == id)
+			return (struct v4l2_ctrl *)msm_venc_ctrls[c].priv;
+	return NULL;
+}
+
 static int try_set_ext_ctrl(struct msm_vidc_inst *inst,
 	struct v4l2_ext_controls *ctrl)
 {
@@ -2156,6 +2167,7 @@ static int try_set_ext_ctrl(struct msm_vidc_inst *inst,
 	struct v4l2_ext_control *control;
 	struct hfi_device *hdev;
 	struct hal_ltrmode ltrmode;
+	struct v4l2_ctrl *cluster;
 	u32 property_id = 0;
 	void *pdata = NULL;
 	struct msm_vidc_core_capability *cap = NULL;
