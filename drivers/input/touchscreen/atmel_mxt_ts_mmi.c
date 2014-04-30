dcfe89075926c3d9e2be868382b084e49d97800f
Konstantin Makariev <hcv867@motorola.com>
2014-04-30 09:46:37 -0500

    input: atmel_mxt_ts_mmi: do not run calibration on resume
    Driver should not force calibration on resume from LPM mode.
    Calibration is taken care of by different objects for both self and
    mutual capacitance.
    
    Change-Id: I33e2ed5e4ac9a3dac2f0e1c791c9d40c6bc01ec3
    Signed-off-by: Konstantin Makariev <hcv867@motorola.com>
    Reviewed-on: http://gerrit.mot.com/631848
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Jeffrey Carlyle <jeff.carlyle@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    SLTApproved: Connie Zhao <czhao1@motorola.com>
    Reviewed-by: Christopher Fries <c.fries@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/input/touchscreen/atmel_mxt_ts_mmi.c
+++ drivers/input/touchscreen/atmel_mxt_ts_mmi.c
@@ -5128,7 +5128,7 @@ static int mxt_input_open(struct input_dev *dev)
 		mxt_regulator_enable(data);
 		mxt_acquire_irq(data);
 	} else if (data->sensor_sleep)
-		mxt_sensor_wake(data, true);
+		mxt_sensor_wake(data, false);
 
 		mxt_do_calibration(data);
 	dev_dbg(dev, "MXT started\n");
@@ -6092,7 +6092,7 @@ static int __devexit mxt_remove(struct i2c_client *client)
 			mxt_regulator_enable(data);
 			mxt_acquire_irq(data);
 		} else if (data->sensor_sleep)
-			mxt_sensor_wake(data, true);
+			mxt_sensor_wake(data, false);
 
 		mutex_unlock(&data->crit_section_lock);
 		dev_dbg(&data->client->dev, "critical section RELEASE\n");
